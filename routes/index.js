161c6bf8c6222d023836e123a198538e2321e0a4
apeeyush <peeyushagarwal1994@gmail.com>
2016-03-12 15:25:07 +0530

    Add basic showcase functionality

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PML Demo' });
+});
+
+/* GET API Request */
+router.get('/api/v1/similar/', function(req, res, next) {
+  url = req.query.url
+  products = [url, url, url]
+  res.render('showcase', { title: 'Showcase', original_url: url, similar_products: products });
 });
 
 module.exports = router;
