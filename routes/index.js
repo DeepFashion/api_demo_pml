e65cce54979c7f9382f15bf343977fd738a88aae
apeeyush <peeyushagarwal1994@gmail.com>
2016-03-13 00:03:10 +0530

    Add explorer functionality

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,19 @@ router.get('/api/v1/similar/', function(req, res, next) {
   res.render('showcase', { title: 'Showcase', original_url: url, similar_products: products });
 });
 
+/* GET NN Request */
+router.get('/explorer/', function(req, res, next) {
+  if(req.query.offset){
+  	offset = parseInt(req.query.offset)
+  }else{
+  	offset=0
+  }
+  var fs = require('fs');
+  var path = require('path');
+  var appDir = path.dirname(require.main.filename);
+  var obj = JSON.parse(fs.readFileSync(appDir+'/../public/data/data.json', 'utf8'));
+  obj = obj.slice(offset, offset+5)
+  res.render('explorer', { title: 'Explorer', data_obj: obj, next_offset: offset+5});
+});
+
 module.exports = router;
